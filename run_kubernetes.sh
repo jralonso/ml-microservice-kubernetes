ae2176566650801104ee7a8a5a688d7aea61370c
juanra <JuanRamon.AlonsoGarcia@zooplus.com>
2020-03-23 23:56:40 +0100

    chore: added commands to run container in kubernetes and forward container port to host

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=jralonso/udacity-devops-mlproject
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run ml-mserv-dev\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels env=ml-mserv-development
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward ml-mserv-dev 8000:80
