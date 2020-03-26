80a4229d8bfc455430a9bc8cfaae3ee57fe9176f
juanra <JuanRamon.AlonsoGarcia@zooplus.com>
2020-03-26 15:54:43 +0100

    chore: added arguments to upload conainer script

--- upload_docker.sh
+++ upload_docker.sh
@@ -3,6 +3,32 @@
 
 # Assumes that an image is built via `run_docker.sh`
 
+# Usage
+# -u <username>
+# -i <docker image>
+# -t <image tag>
+# -p <docker path>
+
+while getopts ":i:u:t:p:" opt; do
+  case $opt in
+    i) dockerimage="$OPTARG"
+    ;;
+    t) dockertag="$OPTARG"
+    ;;
+    u) dockeruser="$OPTARG"
+    ;;
+    p) dockerpath="$OPTARG"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+    ;;
+  esac
+done
+
+#printf "Argument i is %s\n" "$dockerimage"
+#printf "Argument t is %s\n" "$dockertag"
+#printf "Argument u is %s\n" "$dockeruser"
+#printf "Argument p is %s\n" "$dockerpath"
+
 # Step 1:
 # Create dockerpath
 dockerpath=jralonso/udacity-devops-mlproject
@@ -10,8 +36,9 @@ dockerpath=jralonso/udacity-devops-mlproject
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-docker login --username=jralonso
+docker tag $dockerimage:$dockertag $dockerpath:$dockertag
+docker login --username=$dockeruser
 
 # Step 3:
 # Push image to a docker repository
-docker push $dockerpath:latest
+docker push $dockerpath:$dockertag
